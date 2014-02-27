82a3c465c8d8ca403c9d2bfb0fe771fd5bed67e5
maratkanov <alexwitcher@gmail.com>
2014-02-27 14:56:03 +0400

    added views realization , fixed urls parameters

--- src/server.go
+++ src/server.go
@@ -9,16 +9,16 @@ func main() {
 
 	// TODO: регулярку для ID
 	m.Get("/place", getPlaceDetail)
-	m.Get("/favorites", getFavoritsList)
-	m.Get("/route/{id}", getRouteDetail)
+	m.Get("/favorites", getFavoritesList)
+	m.Get("/route/:id", getRouteDetail)
 	m.Get("/history/", getHistoryList)
 
-	m.Post("/ratio/{id}", postRatioDetail)
+	m.Post("/ratio/:id", postRatioDetail)
 
-	m.Delete("/user/{id}", deleteUser)
+	m.Delete("/user/:id", deleteUser)
 
-	m.Put("/ratio/{id}", putRation)
-	m.Put("/user/{id}", putUser)
+	m.Put("/ratio/:id", putRation)
+	m.Put("/user/:id", putUser)
 
 	m.Get("/logout", getLogout)
 	m.Post("/login", getLogin)
--- src/view.go
+++ src/view.go
@@ -1,5 +1,33 @@
 package main
 
-func demo() string {
-	return "Hello suka \n"
+func getRouteDetail(params martini.Params) string {
+	return "getRouteDetail " + params["id"]
+}
+func getFavoritesList() string {
+	return "getFavoritsList "
+}
+func getPlaceDetail() string {
+	return "getPlaceDetail "
+}
+func getHistoryList() string {
+	return "getHistoryList "
+}
+func postRatioDetail(params martini.Params) string {
+	return "postRatioDetail " + params["id"]
+}
+func deleteUser(params martini.Params) string {
+	return "deleteUser " + params["id"]
+}
+func putRation() string {
+	return "putRation "
+}
+func putUser() string {
+	return "putUser "
+}
+
+func getLogout(params martini.Params) string {
+	return "getLogout " + params["id"]
+}
+func getLogin(params martini.Params) string {
+	return "getLogin " + params["id"]
 }
