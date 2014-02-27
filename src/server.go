67a5543f85103f91040b0243d05ffad45341a9c6
sawa2012 <sawa2012@gmail.com>
2014-02-27 13:23:29 +0400

    bugfix: end of line

--- src/server.go
+++ src/server.go
@@ -7,21 +7,21 @@ import (
 func main() {
 	m := martini.Classic()
 	m.Get("/", func() string {
-		return "Hello world - GET"
+		return "Hello world - GET \n"
 	})
 
 	m.Post("/", func() string {
-		return "Hello world - POST"
+		return "Hello world - POST \n"
 		// create something
 	})
 
 	m.Put("/", func() string {
-		return "Hello world - PUT"
+		return "Hello world - PUT \n"
 		// replace something
 	})
 
 	m.Delete("/", func() string {
-		return "Hello world - DELETE"
+		return "Hello world - DELETE \n"
 		// destroy something
 	})
 
