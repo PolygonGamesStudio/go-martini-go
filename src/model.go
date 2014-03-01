89d27002695e57577db551df6428be5059cc111f
maratkanov <alexwitcher@gmail.com>
2014-03-01 20:05:21 +0400

    added models for orm

---
+++ src/model.go
@@ -0,0 +1,43 @@
+type Place struct {
+	Id int64
+	Description string
+	PriceCategory int64
+	Logo string
+	EndPointAddress string
+	Creator
+	CoordinatesGPS string
+	IsActive bool
+	Route string
+	Category
+}
+
+type Creator struct {
+	Id int64
+	Login string
+	Password string
+}
+
+type User struct {
+	Id int64
+	Login string
+	Password string
+}
+
+type RouteNodes struct {
+	Id int64
+	Description string
+}
+
+type Category struct {
+	Id int64
+	Name string
+	BriefDescription string	
+	logo string
+}
+
+type UserPlaceMTM struct {
+	User
+	Place
+	Ratio int8
+	Feedback string
+}
